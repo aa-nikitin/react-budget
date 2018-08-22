4ed29ecb07af0ee0d6d7c3539bd8b38734ee0e5b
aa-nikitin <a.nikitin91@mail.ru>
2018-08-22 17:52:40 +0300

    added date management

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
+import styled from 'styled-components';
+
+const DateButton = styled.button`
+  color: white;
+  border: 1px solid white;
+  border-radius: 50%;
+  background-color: transparent;
+  width: 32px;
+  height: 32px;
+  margin: 3px;
+  cursor: pointer;
+  text-align: center;
+`;
+
+const DateLine = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: moment()
+    }
+  }
+
+  handleAddDay = () => {
+    this.setState({
+      date: this.state.date.add(1, 'day')
+    });
+  }
+
+  handleSubstractDay = () => {
+    this.setState({
+      date: this.state.date.subtract(1, 'day')
+    });
+  }
+
   render() {
+    const {date} = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <section>
+        <header>
+          <h1>Реактивный бюджет</h1>
+          <DateLine>
+            <DateButton onClick={this.handleSubstractDay}>-</DateButton>
+            <p>{date.format('DD.MM.YYYY')}</p>
+            <DateButton onClick={this.handleAddDay}>+</DateButton>
+          </DateLine>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </section>
     );
   }
 }
